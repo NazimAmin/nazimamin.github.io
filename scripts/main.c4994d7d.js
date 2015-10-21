2910078894912f1323bdd6ccabc1615dbdc676c1
nazim <amin.nazim13@gmail.com>
2015-10-21 02:15:16 -0400

    scroll

--- scripts/main.c4994d7d.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){particlesJS.load("welcome-header-container","scripts/particlesjs-config.json"),$(".fa-heart, .fa-code").bind("mouseenter mouseleave click",function(){$(this).toggleClass("animated rubberBand")})});
